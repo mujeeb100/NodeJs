a83c56ffac956b61c8acfe1d51eccce8b664827e
mujeeb ansari <muzeeb100@yahoo.co.in>
2022-10-22 11:50:28 +0530

    adding a note in the json file manualy

--- notes.js
+++ notes.js
@@ -1,5 +1,38 @@
-const getNotes = function () {
-    return 'Your notes...'
-}
+const fs = require("fs");
 
-module.exports = getNotes
+const getNotes = function () {
+  return "Your notes...";
+};
+
+const addNote = function (title, body) {
+  const notes = loadNotes();
+
+  notes.push({
+    title: title,
+    body: body,
+  });
+
+  saveNOTES(notes);
+};
+
+const saveNOTES = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = function () {
+  // JAVASCRIPT TRY AND CATCH CODE BLOCK
+
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+};
